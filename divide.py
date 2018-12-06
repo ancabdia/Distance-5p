51eae1719ffe93f9f865bf6a37a03f112cbc54f3
Alberto Ramos Sánchez <36385379+bertoramos@users.noreply.github.com>
2018-12-06 18:36:44 +0000

    Llama a la función iterativa desde otra
    En clase nos daba un número muy grande porque cada vez que se llamaba recursivamente hacía lo de consultar si estaba el resultado en el mapa.
    Lo cambié poniendo eso fuera, en otra función.

--- divide.py
+++ divide.py
@@ -1,32 +1,36 @@
 dp={}
 
 def divide (str1, str2, m, n):
-    if (str1 and str2) in dp:
+    result = 0
+    if m == 0:
+        dp[str1,str2] = n
+        return n
+    if n == 0:
+        dp[str1, str2] = m;
+        return m
+    if str1[m-1] == str2[n-1]:
+        result = divide(str1, str2, m-1, n-1);
+        dp[str1, str2] = result;
+    else:
+        result = 1 + min(divide(str1, str2, m, n-1),
+					 divide(str1, str2, m-1, n),
+					 divide(str1, str2, m-1, n-1));
+        dp[str1, str2] = result;
+    return result
+
+def edit_distance(str1,str2,m,n):
+    if (str1,str2) in dp:
         return dp[str1,str2]
     else:
-        result = 0
-        if m == 0:
-            dp[str1,str2] = n
-            return n
-        if n == 0:
-            dp[str1, str2] = m;
-            return m
-        if str1[m-1] == str2[n-1]:
-            result = divide(str1, str2, m-1, n-1);
-            dp[str1, str2] = result;
-        else:
-            result = 1 + min(divide(str1, str2, m, n-1),
-                         divide(str1, str2, m-1, n),
-                         divide(str1, str2, m-1, n-1));
-            dp[str1, str2] = result;
-        return result
-
+        return divide(str1,str2,m,n)
     
+
 # Driver program 
-str1 = "sunday"
-str2 = "saturday"
+str1 = "potato"
+str2 = "petete"
   
-print(divide(str1, str2, len(str1), len(str2)) )
-print(divide(str1, str2, len(str1), len(str2)) )
+print(edit_distance(str1, str2, len(str1), len(str2)) )
+print(dp)
+print(edit_distance(str1, str2, len(str1), len(str2)) )
 # This code is contributed by Bhavya Jain 
 
