8dd845c0a0c674b86451b067b215a54a9d00cb44
Andrés Cabrera Diaz <ancabdia.contact@gmail.com>
2018-12-12 13:42:30 +0000

    Version fina
    Permite leer desde fichero de texto

--- divide.py
+++ divide.py
@@ -1,4 +1,8 @@
+import sys #libreria python para usar argumentos por consola
+
 dp={}
+
+
 
 def divide (str1, str2, m, n):
     result = 0
@@ -26,11 +30,24 @@ def edit_distance(str1,str2,m,n):
     
 
 # Driver program 
-str1 = "potato"
-str2 = "petete"
-  
-print(edit_distance(str1, str2, len(str1), len(str2)) )
-print(dp)
-print(edit_distance(str1, str2, len(str1), len(str2)) )
+#str1 = "potato"
+#str2 = "petete"
+
+#1- Leemos el fichero de datos
+file = open(sys.argv[1], "r") #abrir fichero en modo lectura
+palabras = [] #array para guardar las palabras
+while(1):
+	linea = file.readline().strip() # palabra1;palabra2
+	if not linea:
+		break
+	palabra = linea.split(";")
+	palabras.append(palabra)
+
+for i in palabras: #Mostrar palabras y resultado
+	str1 = i[0]
+	str2 = i[1]
+	print("Palabra "+ str(str1) +" " + str(str2) + "= " + str(edit_distance(str1, str2, len(str1), len(str2))))
+
+#print(dp) Imprimir todo el contenedor
 # This code is contributed by Bhavya Jain 
 
