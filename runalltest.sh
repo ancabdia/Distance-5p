b6383eb7d806f9e664767d22f5139fc765f1c26f
Alberto Ramos Sánchez <36385379+bertoramos@users.noreply.github.com>
2018-12-14 15:57:14 +0000

    Runalltest.sh

---
+++ runalltest.sh
@@ -0,0 +1,5 @@
+echo "-------------------- Memoization --------------------"
+python divide.py fichero.txt
+echo ""
+echo "-------------------- Tabulation  --------------------"
+python divide_tab.py fichero.txt
